2a6f813724af00379c7cb50460fe4e90561c334f
samwelkinuthia <samwuelkinuthia@gmail.com>
2022-08-02 19:52:40 +0300

    added endpoints

---
+++ src/services/CommunicationsAPI.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+let username = "admin";
+let password = "password";
+const encodedToken = window.btoa(username + ":" + password);
+
+var headers = {
+  Authorization: "Basic " + encodedToken,
+  "Content-Type": "application/json",
+};
+
+export const communicationService = axios.create({
+  headers: headers,
+  timeout: 20000,
+});
+
+export const communicationBaseUrl = "http://142.93.231.97:8083";
