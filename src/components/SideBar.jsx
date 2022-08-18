2f6e1f9421c08bbc62e63a7b4a84ee47289bf2e6
Kelvin Thuku <kelvinthuku@Lomans-MacBook-Pro.local>
2022-08-18 21:45:09 +0300

    removed auctioneer data

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -111,10 +111,7 @@ function SideBar() {
                             <ul className="sub-menu" aria-expanded="false">
                                 <li><NavLink to="/adminlist">Users List</NavLink></li>
                                 <li><NavLink to="/addadmin">Add a User</NavLink></li>
-                              
                                 <li><NavLink to="/usertypes"> UserTypes</NavLink></li>
-                                <li><NavLink to="/addauctioneers"> Add Auctioneers</NavLink></li>
-                                <li><NavLink to="/auctioneers">Auctioneers</NavLink></li>
                             </ul>
                         </li>
                         {/* <li>
