cc22f345d2352fd8d25de2cd2e503180a786622b
samwelkinuthia <samwuelkinuthia@gmail.com>
2022-08-23 10:50:09 +0300

    gc pagination

--- src/pages/messanger/BulkMessagesList.jsx
+++ src/pages/messanger/BulkMessagesList.jsx
@@ -1,41 +1,26 @@
 /* global $*/
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import requestsServiceService from "../../services/requestsService.service";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 
 export default function BulkMessagesList() {
   const [messages, setmessages] = useState([]);
   const [page, setPage] = useState(0);
-  const [size, setSize] = useState(10);
+  const [size, setSize] = useState(5);
   const [pageCount, setPageCount] = useState(1);
-  
+  const [totalMessages, setTotalMessages] = useState([]);
+
   const sortSize = (e) => {
     setSize(e.target.value);
   };
   const [itemOffset, setItemOffset] = useState(0);
-  
+
   useEffect(() => {
-    getBulkMessages()
+    getBulkMessages();
+    console.log(pageCount);
   }, [itemOffset, size]);
-  
-  // Invoke when user click to request another page.
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * 10) % messages.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
-    setItemOffset(newOffset);
-  };
-  
-  useEffect(() => {
-    setPageCount(Math.ceil(messages.length / size))
-  }, [messages, size])
-  
-  useEffect(() => {
-    console.log(pageCount)
-  }, [pageCount])
-  
+
   const getBulkMessages = () => {
     var data = {};
     var ds = [];
@@ -44,34 +29,38 @@ export default function BulkMessagesList() {
         ds.push(
           Object.assign(
             data,
-            {d: JSON.parse(item.data)},
-            {bulkRef: item.bulkReference},
-            {done: item.done}
+            { d: JSON.parse(item.data) },
+            { bulkRef: item.bulkReference },
+            { done: item.done }
           )
         );
       });
+      setTotalMessages(ds);
       const endOffset = itemOffset + size;
       setmessages(ds.slice(itemOffset, endOffset));
-      setPageCount(Math.ceil(messages.length / messages));
+      setPageCount(Math.ceil(ds.length / size));
     });
   };
-  
-  // const handlePageClick = (data) => {
-  //   let d = data.selected;
-  //   setPage(d);
-  // };
-  
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * size) % totalMessages.length;
+    console.log(
+      `User requested page number ${event.selected}, offset by ${newOffset}`
+    );
+    setItemOffset(newOffset);
+    setPage(event.selected);
+  };
+
   const [activeMessage, setActiveMessage] = useState({});
-  
+
   function getOneBulkmessage(id) {
     setActiveMessage(id);
   }
-  
+
   useEffect(() => {
     getBulkMessages();
     console.log(activeMessage);
   }, [activeMessage]);
-  
+
   return (
     <>
       <div className="page-content">
@@ -82,7 +71,7 @@ export default function BulkMessagesList() {
                 <h4 className="mb-sm-0 font-size-18 text-capitalize">
                   Bulk Messaging
                 </h4>
-                
+
                 <div className="page-title-right">
                   <ol className="breadcrumb m-0">
                     <li className="breadcrumb-item">
@@ -103,8 +92,7 @@ export default function BulkMessagesList() {
                 <div className="row">
                   <div className="col-12">
                     <div className="card">
-                      <div
-                        className="card-header bg-white pt-0 pr-0 p-0 d-flex justify-content-between align-items-center w-100 border-bottom">
+                      <div className="card-header bg-white pt-0 pr-0 p-0 d-flex justify-content-between align-items-center w-100 border-bottom">
                         <div
                           className="btn-toolbar p-3 d-flex justify-content-between align-items-center w-100"
                           role="toolbar"
@@ -113,8 +101,7 @@ export default function BulkMessagesList() {
                             All Bulk Messages
                           </h4>
                         </div>
-                        <div
-                          className="card-header bg-white pt-0 pr-0 p-0 d-flex justify-content-between align-items-center w-100 border-bottom">
+                        <div className="card-header bg-white pt-0 pr-0 p-0 d-flex justify-content-between align-items-center w-100 border-bottom">
                           <div
                             className="btn-toolbar p-3 d-flex justify-content-between align-items-center w-100"
                             role="toolbar"
@@ -128,8 +115,8 @@ export default function BulkMessagesList() {
                                   data-bs-toggle="modal"
                                   data-bs-target="#add-new-client"
                                 >
-                                  <i className="mdi mdi-plus label-icon"></i> Create bulk
-                                  Message
+                                  <i className="mdi mdi-plus label-icon"></i>{" "}
+                                  Create bulk Message
                                 </button>
                               </Link>
                             </div>
@@ -143,104 +130,104 @@ export default function BulkMessagesList() {
                             id="datatable-buttons"
                           >
                             <thead className="table-light">
-                            <tr className="table-light">
-                              <th>Bulk Reference</th>
-                              <th>Recipient</th>
-                              <th>Message Type</th>
-                              <th>Communication Channel</th>
-                              <th>Who to Charge</th>
-                              <th>Time Period</th>
-                              <th className="text-right">Actions</th>
-                            </tr>
+                              <tr className="table-light">
+                                <th>Bulk Reference</th>
+                                <th>Recipient</th>
+                                <th>Message Type</th>
+                                <th>Communication Channel</th>
+                                <th>Who to Charge</th>
+                                <th>Time Period</th>
+                                <th className="text-right">Actions</th>
+                              </tr>
                             </thead>
                             <tbody>
-                            {messages.length > 0 &&
-                              messages?.map((message, index) => (
-                                <tr data-id={index} key={index}>
-                                  <td>{message.bulkRef}</td>
-                                  <td>{message.d?.sendTo}</td>
-                                  <td>{message.d?.messageKind}</td>
-                                  <td>{message.d?.messageType}</td>
-                                  <td>
-                                    {message.d?.whoToCharge === ""
-                                      ? "N/A"
-                                      : message.d?.whoToCharge}
-                                  </td>
-                                  <td>{message.d?.period + " days"}</td>
-                                  <td>
-                                    <div className="d-flex justify-content-end">
-                                      {/*<button type="button"*/}
-                                      {/*        className="btn btn-primary btn-sm waves-effect waves-light text-nowrap me-3"*/}
-                                      {/*        // onClick={() => getOnemessage(message?.transaction.transactionId)}*/}
-                                      {/*        >Receive Payment*/}
-                                      {/*</button>*/}
-                                      <div className="dropdown">
-                                        <a
-                                          className="text-muted font-size-16"
-                                          role="button"
-                                          data-bs-toggle="dropdown"
-                                          aria-haspopup="true"
-                                        >
-                                          <i className="bx bx-dots-vertical-rounded"></i>
-                                        </a>
-                                        <div className="dropdown-menu dropdown-menu-end ">
+                              {messages.length > 0 &&
+                                messages?.map((message, index) => (
+                                  <tr data-id={index} key={index}>
+                                    <td>{message.bulkRef}</td>
+                                    <td>{message.d?.sendTo}</td>
+                                    <td>{message.d?.messageKind}</td>
+                                    <td>{message.d?.messageType}</td>
+                                    <td>
+                                      {message.d?.whoToCharge === ""
+                                        ? "N/A"
+                                        : message.d?.whoToCharge}
+                                    </td>
+                                    <td>{message.d?.period + " days"}</td>
+                                    <td>
+                                      <div className="d-flex justify-content-end">
+                                        {/*<button type="button"*/}
+                                        {/*        className="btn btn-primary btn-sm waves-effect waves-light text-nowrap me-3"*/}
+                                        {/*        // onClick={() => getOnemessage(message?.transaction.transactionId)}*/}
+                                        {/*        >Receive Payment*/}
+                                        {/*</button>*/}
+                                        <div className="dropdown">
                                           <a
-                                            className="dropdown-item cursor-pointer"
-                                            onClick={() => {
-                                              getOneBulkmessage();
-                                            }}
+                                            className="text-muted font-size-16"
+                                            role="button"
+                                            data-bs-toggle="dropdown"
+                                            aria-haspopup="true"
                                           >
-                                            <i className="font-size-15 mdi mdi-eye me-3 "></i>
-                                            View
+                                            <i className="bx bx-dots-vertical-rounded"></i>
                                           </a>
-                                          <a className="dropdown-item">
-                                            <i className="font-size-15 mdi mdi-printer me-3 "></i>
-                                            Print
-                                          </a>
-                                          <a
-                                            className="dropdown-item cursor-pointer"
-                                            // onClick={() => {
-                                            //   handleModeChange("Email");
-                                            //   handleClicked(message, "Email");
-                                            // }}
-                                          >
-                                            <i className="font-size-15 mdi mdi-email me-3 "></i>
-                                            Email Tenant
-                                          </a>
-                                          <a
-                                            className="dropdown-item cursor-pointer"
-                                            // onClick={() => {
-                                            //   handleModeChange("SMS");
-                                            //   handleClicked(message, "SMS");
-                                            // }}
-                                          >
-                                            <i className="font-size-15 mdi mdi-chat me-3"></i>
-                                            Send as SMS
-                                          </a>
+                                          <div className="dropdown-menu dropdown-menu-end ">
+                                            <a
+                                              className="dropdown-item cursor-pointer"
+                                              onClick={() => {
+                                                getOneBulkmessage();
+                                              }}
+                                            >
+                                              <i className="font-size-15 mdi mdi-eye me-3 "></i>
+                                              View
+                                            </a>
+                                            <a className="dropdown-item">
+                                              <i className="font-size-15 mdi mdi-printer me-3 "></i>
+                                              Print
+                                            </a>
+                                            <a
+                                              className="dropdown-item cursor-pointer"
+                                              // onClick={() => {
+                                              //   handleModeChange("Email");
+                                              //   handleClicked(message, "Email");
+                                              // }}
+                                            >
+                                              <i className="font-size-15 mdi mdi-email me-3 "></i>
+                                              Email Tenant
+                                            </a>
+                                            <a
+                                              className="dropdown-item cursor-pointer"
+                                              // onClick={() => {
+                                              //   handleModeChange("SMS");
+                                              //   handleClicked(message, "SMS");
+                                              // }}
+                                            >
+                                              <i className="font-size-15 mdi mdi-chat me-3"></i>
+                                              Send as SMS
+                                            </a>
+                                          </div>
                                         </div>
                                       </div>
-                                    </div>
-                                  </td>
-                                </tr>
-                              ))}
+                                    </td>
+                                  </tr>
+                                ))}
                             </tbody>
                             <tfoot className="table-dark">
-                            <tr>
-                              <th
-                                className="text-capitalize text-nowrap"
-                                colSpan="3"
-                              >
-                                {messages && messages.length} messages
-                              </th>
-                              <td
-                                className="text-nowrap text-right"
-                                colSpan="6"
-                              >
+                              <tr>
+                                <th
+                                  className="text-capitalize text-nowrap"
+                                  colSpan="3"
+                                >
+                                  {messages && messages.length} messages
+                                </th>
+                                <td
+                                  className="text-nowrap text-right"
+                                  colSpan="6"
+                                >
                                   <span className="fw-semibold">
                                     {/*{formatCurrency.format(total())}*/}
                                   </span>
-                              </td>
-                            </tr>
+                                </td>
+                              </tr>
                             </tfoot>
                           </table>
                         </div>
@@ -253,9 +240,9 @@ export default function BulkMessagesList() {
                               value={size}
                               onChange={(e) => sortSize(e)}
                             >
+                              <option value={parseInt(5)}>5</option>
                               <option value={parseInt(10)}>10</option>
-                              <option value={parseInt(30)}>30</option>
-                              <option value={parseInt(50)}>50</option>
+                              <option value={parseInt(20)}>20</option>
                             </select>
                           </div>
                           {pageCount !== 0 && (
@@ -263,16 +250,16 @@ export default function BulkMessagesList() {
                               {" "}
                               showing page{" "}
                               <span className="text-green-700 text-opacity-100 font-bold text-sm">
-                              {page + 1}
-                            </span>{" "}
+                                {page + 1}
+                              </span>{" "}
                               of{" "}
                               <span className="text-sm font-bold text-black">
-                              {pageCount}
-                            </span>{" "}
+                                {pageCount}
+                              </span>{" "}
                               pages
                             </p>
                           )}
-                          
+
                           {pageCount !== 0 && (
                             <ReactPaginate
                               previousLabel={"prev"}
@@ -304,13 +291,12 @@ export default function BulkMessagesList() {
               <></>
             )}
           </div>
-          
+
           <footer className="footer ">
             <div className="container-fluid ">
               <div className="row ">
                 <div className="col-sm-6 ">
-                  <script>document.write(new Date().getFullYear())</script>
-                  ©
+                  <script>document.write(new Date().getFullYear())</script>©
                   RevenueSure
                 </div>
                 <div className="col-sm-6 ">
